7cf8c41d8d2422c110f714bed9a776fe860e8f49
SomnambulistCyber <70433163+SomnambulistCyber@users.noreply.github.com>
2025-07-15 14:35:20 +0100

    added game clock to limit resource hogging and FPS

--- asteroids/main.py
+++ asteroids/main.py
@@ -2,12 +2,14 @@ import pygame
 from constants import *
 
 pygame.init()
+dt = 0
 
 def main():
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +19,7 @@ def main():
                 
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
