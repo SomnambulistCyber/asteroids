3849a58b8c692cb2637b500409d78afcc69eb538
SomnambulistCyber <70433163+SomnambulistCyber@users.noreply.github.com>
2025-07-15 15:44:54 +0100

    add a player and hitbox

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,23 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
-pygame.init()
-dt = 0
+
 
 def main():
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+
+    player_pos = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+       
         pygame.Surface.fill(screen, (0,0,0))
+        player_pos.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
